1b6a72dfd311939a4e9490dbed3f439b95c9ffa6
Kriszyle Hugo <kriszylehugo32@gmail.com>
2026-03-29 11:26:56 +0800

    changes

--- resources/views/layout/reservation.app.blade.php
+++ resources/views/layout/reservation.app.blade.php
@@ -1,25 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Facility Reservation System</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-</head>
-<body>
-    <div class="dashboard-container">
-        @include('partial._sidebar')
+@extends('layout.app')
 
-        <main class="main-content">
-            @include('partial._header')
-
-            <div class="content-area">
-                @yield('content')
-            </div>
-
-            @include('partial._footer')
-        </main>
-    </div>
-</body>
-</html>
+@section('content')
+    @include('partial._reservation')
+@endsection
